0c51b147ab95e076f9449f5fe7572594b60bfa84
mrnitj <nithinraj982@gmail.com>
2023-11-30 19:13:00 +0530

    update

--- pattern&loops/HollowSquare.js
+++ pattern&loops/HollowSquare.js
@@ -3,16 +3,13 @@ let string = "";
 
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-        if (i === 0 || i === n - 1) {
+        if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
             string += "*";
         } else {
-            if (j === 0 || j === n - 1) {
-                string += "*";
-            } else {
-                string += " ";
-            }
+            string += " ";
         }
     }
     string += "\n";
 }
+
 console.log(string);
