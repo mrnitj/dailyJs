698fdfd66ef5dabaff761884e5a250a9c9d71c45
mrnitj <nithinraj982@gmail.com>
2023-12-04 16:45:25 +0530

    update

--- pattern&loops/LeftTriangle.js
+++ pattern&loops/LeftTriangle.js
@@ -1,16 +1,15 @@
-let n =5;
-let string = '';
+let n = 5;
+let string = "";
 
-for(let i=1; i<=n; i++){
-    for(let j=1; j<=i; j++){
-        string+='*';
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        string += "*";
     }
-    for(k=i; k<=n-1; k++){
-        string+=' '
+    for (k = i; k <= n - 1; k++) {
+        string += " ";
     }
 
-    string+='\n'
-
+    string += "\n";
 }
 
 console.log(string);
